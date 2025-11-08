1716d1f3ea080e30d852caef001e1ef2055f81fb
eric <eric.guo171@gmail.com>
2025-11-08 14:29:23 -0500

    basic navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import Navbar from '@/components/Navbar';
 import LoginPage from '@/components/LoginPage';
 import RegisterPage from '@/components/RegisterPage';
 
 export default function App() {
   return (
     <>
+    <Navbar />
+    <hr/>
     <LoginPage />
     <hr/>
     <RegisterPage />
